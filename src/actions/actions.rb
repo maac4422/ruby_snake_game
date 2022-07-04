802123db0907c070090e3a9030165418aa09a42e
Miguel Angel Arenas Correa <miguelarenas9806@gmail.com>
2022-07-03 19:14:48 -0500

    Change direction logic

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -1,6 +1,6 @@
 module Actions
   def self.move_snake(state)
-    next_direction = state.next_direction
+    next_direction = state.curr_direction
     next_position = calc_next_position(state)
     puts(next_position)
     if position_is_valid?(state, next_position)
@@ -10,11 +10,20 @@ module Actions
     end
   end
 
+  def self.change_direction(state, direction)
+    if next_direction_is_valid?(state, direction)
+      state.curr_direction = direction
+    else
+      puts "invalid direction"
+    end
+    state
+  end
+
   private
   
   def self.calc_next_position(state)
     current_position = state.snake.positions.first
-    case state.next_direction
+    case state.curr_direction
       when Model::Direction::UP
         return Model::Coord.new(
           current_position.row - 1,
@@ -51,5 +60,19 @@ module Actions
   def self.end_game(state)
     state.game_finished = true
     state
+  end
+
+  def self.next_direction_is_valid?(state,direction)
+    case state.curr_direction
+    when Model::Direction::UP
+      return true if direction != Model::Direction::DOWN
+    when Model::Direction::DOWN
+      return true if direction != Model::Direction::UP
+    when Model::Direction::RIGHT
+      return true if direction != Model::Direction::LEFT
+    when Model::Direction::LEFT
+      return true if direction != Model::Direction::RIGHT
+    end
+    return false
   end
 end
