1a65d8a9a4294f0e1c6031a76dfc842a8289fb0e
Miguel Angel Arenas Correa <miguelarenas9806@gmail.com>
2022-07-03 19:49:07 -0500

    Generate random food

--- test/actions_test.rb
+++ test/actions_test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/mock"
 require_relative "../src/actions/actions"
 require_relative "../src/model/state"
 
@@ -67,7 +68,7 @@ class ActionTest < Minitest::Test
   end
 
   def test_snake_grow
-    @initial_state = Model::State.new(
+    initial_state = Model::State.new(
       Model::Snake.new([
         Model::Coord.new(1,1),
         Model::Coord.new(0,1)
@@ -78,11 +79,40 @@ class ActionTest < Minitest::Test
       false
     )
 
-    actual_state = Actions::move_snake(@initial_state)
+    actual_state = Actions::move_snake(initial_state)
     assert_equal(actual_state.snake.positions, [
         Model::Coord.new(2,1),
         Model::Coord.new(1,1),
         Model::Coord.new(0,1)
     ])
+  end
+
+  def test_generate_food
+    initial_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
+      ]),
+      Model::Food.new(2,1),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+
+    expected_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(2,1),
+        Model::Coord.new(1,1),
+        Model::Coord.new(0,1)
+      ]),
+      Model::Food.new(0,0),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+    Actions.stub(:rand,0) do
+      actual_state = Actions::move_snake(initial_state)
+      assert_equal(actual_state, expected_state)
+    end
   end
 end
